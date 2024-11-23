583d7e0bcd53d62a8e35e7018d8e506b55f37a11
Eduardo Rigo <eduardorigom@gmail.com>
2024-11-22 23:02:58 -0300

    fix: SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,45 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Ye Quotes",
-  description: "Powered by Kanye.REST",
-};
+  generator: 'Next.js',
+  title: 'Kanye Quotes',
+  description: 'Powered by Kanye.REST',
+  applicationName: 'NASA Dashboard',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript', 'typescript', 'nextjs', 'frontend', 'developer', 'front end', 'programmer'],
+  authors: [{ name: 'Eduardo Rigo', url: 'https://eduardev.com' }],
+  creator: 'Eduardo Rigo',
+  publisher: 'Eduardo Rigo',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: 'Kanye Quotes',
+    description: 'Powered by Kanye.REST',
+    url: 'https://kanye.eduardev.com',
+    siteName: 'Kanye Quotes',
+    images: [
+      {
+        url: 'https://www.kanye.eduardev.com/images/bg.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://www.kanye.eduardev.com/images/bg-g.png',
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+  },
+}
+
 
 export default function RootLayout({
   children,
